06bfa158c7c1369f8eb4bd31d533913bd78683a9
pilasilda <pilasilda96@hotmail.com>
2019-03-19 18:08:18 +0100

    started on login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,9 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/login_page.dart';
 
 class LoginPage extends StatefulWidget{
   static String tag = 'Loginpage';
 
-
   @override
-  _LoginPageState createState() => new _LoginPageState
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>{
+  @override
+  Widget build(BuildContext context){
+
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('lib/wwf-logo-design.jpg'),
+      )
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: 'pilasilda96@hotmail.com',
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0,10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0)
+        ),
+      ),
+    );
+
+    final passord = TextFormField(
+        autofocus: false,
+        initialValue: 'some password',
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'passowrd',
+          contentPadding: EdgeInsets.fromLTRB(20.0,10.0, 20.0, 10.0),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0)),
+        ),
+    );
+
+    return new Container();
+    }
 }
