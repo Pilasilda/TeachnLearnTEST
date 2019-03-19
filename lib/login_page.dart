19fab76c79dd751e65072c66cb81e1c2a59dffae
pilasilda <pilasilda96@hotmail.com>
2019-03-19 17:19:42 +0100

    starte on: Loginpage

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatefulWidget{
   static String tag = 'Loginpage';
 
+
   @override
   _LoginPageState createState() => new _LoginPageState
 }
