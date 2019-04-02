909669f55b6c400ecb8dcfbe5dec5f5ba5323185
pilasilda <pilasilda96@hotmail.com>
2019-04-02 17:31:28 +0200

    loginpage finished, problem with uploading picture

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'login_page.dart';
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
---
+++ lib/mainpage.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void mainpage() => runApp new MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kodeversitas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
+      ),
+      home: LoginPage(),
+      routes: routes,
+    );
+  }
+}
+
+class Mainpage extends StatefulWidget{
+  static String tag = 'Mainpage';
+
+  @override
+  _Mainpage createState()  => new _Mainpage();
+}
+
+class _Mainpage extends State<Mainpage>{
+  @override
+  Widget build(BuildContext context){
+
+    final rajani = Hero(
+      tag : 'hero',
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 48.0,
+            child: new Image.asset("lib/wwf-logo-design.jpg"),
+    ),
+    );
+  }
+}
