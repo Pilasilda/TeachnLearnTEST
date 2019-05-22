530c3a7f18583b7610e0f0b3760bfe2a7b8fb86c
Meimona Hakim <meimonah@student.matnat.uio.no>
2019-05-22 16:22:44 +0200

    almost finished email and password page

--- lib/email_pass_page.dart
+++ lib/email_pass_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'who_are_you_page.dart';
 
 class EmailPassPage extends StatefulWidget{
   static String tag = 'EmailPassPage';
@@ -8,17 +9,12 @@ class EmailPassPage extends StatefulWidget{
 }
 
 class _EmailPassPageState extends State<EmailPassPage>{
-  bool _maleIsChecked = false;
-  bool _femaleIsChecked = false;
-  void onChanged(bool value) {
-    setState((){
-      _maleIsChecked = value;
-      _femaleIsChecked = value;
-    });
-  }
+
+
 
   @override
   Widget build(BuildContext context){
+
 
     final logo = Hero(
       tag: 'hero',
@@ -28,7 +24,6 @@ class _EmailPassPageState extends State<EmailPassPage>{
         child: new Image.asset("lib/wwf-logo-design.jpg"),
       ),
     );
-
 
     final nextButton = Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
@@ -43,18 +38,20 @@ class _EmailPassPageState extends State<EmailPassPage>{
             child: Text('Next', style: TextStyle(color: Colors.white)),
             onPressed: () {
               Navigator.push(context,
-                  new MaterialPageRoute(builder: (context) => new WhoAreYouPage())
+                  new MaterialPageRoute(builder: (context) => new EmailPassPage())
               );
             }
         ),
       ),
     );
 
-    final name = TextFormField(
+
+
+    final email = TextFormField(
       keyboardType: TextInputType.emailAddress,
       autofocus: false,
       decoration: InputDecoration(
-        hintText: 'Name',
+        hintText: 'Email',
         contentPadding: EdgeInsets.fromLTRB(20.0,10.0, 20.0, 10.0),
         border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(32.0)
@@ -62,29 +59,39 @@ class _EmailPassPageState extends State<EmailPassPage>{
       ),
     );
 
-    final genderCheckbox = new Container(
-      padding: new EdgeInsets.all(14.0),
-      child: new Center(
-        child:
-        new Column(
-            children: <Widget>[
-              new Row(
-                  children: <Widget>[
-                    new Text('Dog'),
-                    new Checkbox(value: _maleIsChecked, onChanged: (bool value){onChanged(value);}),
-                  ]
-              ),
-              new Row(
-                  children: <Widget>[
-                    new Text('Donkey'),
-                    new Checkbox(value: _femaleIsChecked, onChanged: (bool value){onChanged(value);}),
-                  ]
-              ),
-            ]
+    final password = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0,10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32.0)
         ),
       ),
     );
 
+    final confPass = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Confirm password',
+        contentPadding: EdgeInsets.fromLTRB(20.0,10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32.0)
+        ),
+      ),
+    );
+
+    final terms = FlatButton(
+      child: Text('Terms and conditions',
+          style: TextStyle(color: Colors.black54)),
+      onPressed: (){
+        /*Navigator.push(context,
+            new MaterialPageRoute(builder: (context) => new RegistrationPage())
+        );*/
+      },
+    );
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -95,8 +102,10 @@ class _EmailPassPageState extends State<EmailPassPage>{
           children: <Widget>[
             logo,
             SizedBox(height: 24.0),
-            name,
-            genderCheckbox,
+            email,
+            password,
+            confPass,
+            terms,
             nextButton
           ],
         ),
